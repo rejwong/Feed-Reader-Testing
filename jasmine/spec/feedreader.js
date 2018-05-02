eee680f7c507a742be8a061d465bab4c39f44e5d
rejwong <rebecca.ej.wong@gmail.com>
2018-05-02 15:22:11 +0100

    Project updated - comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -2,25 +2,22 @@
     For reference: https://jasmine.github.io/2.1/introduction
  *
  * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
+ * all of the tests that will be run against the application.
  */
 
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
+/* All of our tests have been placed within the $() function,
+ * since some of these tests may require DOM elements. This
+ * ensures they don't run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
+    /* This is the first test suite - a test suite contains
     * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
+    * feeds definitions, the allFeeds variable in the application.
     */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
+        /* This is the first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
+         * empty. 
          */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
@@ -28,52 +25,39 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Tests loops through each feed
          * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
+         * and that the URL is not empty. 
          */
-        it('URL is defined', function() {
+        it('URL is defined and is not empty', function() {
             allFeeds.forEach(function(feed) {
                 expect(feed.url).toBeDefined();
-            });
-        });
-
-        it('URL is not empty', function() {
-            allFeeds.forEach(function(feed) {
                 expect(feed.url).not.toBe(null);
             });
         });
 
-
-        /* TODO: Write a test that loops through each feed
+        /* Test loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-        it('name is defined', function() {
+        it('name is defined and is not empty', function() {
             allFeeds.forEach(function(feed) {
                 expect(feed.name).toBeDefined();
-            });
-        });
-    
-        it('name is not empty', function() {
-            allFeeds.forEach(function(feed) {
                 expect(feed.name).not.toBe(null);
             });
         });
     });
-//     /* TODO: Write a new test suite named "The menu" */
+//     /* Created a new test suite named "The menu" */
     describe('The menu', function() {
-//     // //     /* TODO: Write a test that ensures the menu element is
-//     // //      * hidden by default. You'll have to analyze the HTML and
-//     // //      * the CSS to determine how we're performing the
-//     // //      * hiding/showing of the menu element.
+//     // //     /* Test ensures the menu element is
+//     // //      * hidden by default.
 //     // //      */
         it('menu is hidden', function() {
             expect($('body').hasClass('menu-hidden')).toBe(true)
         });
-//     // //      /* TODO: Write a test that ensures the menu changes
-//     // //       * visibility when the menu icon is clicked. This test
-//     // //       * should have two expectations: does the menu display when
+//     // //      /* Test ensures the menu changes
+//     // //       * visibility when the menu icon is clicked. The test
+//     // //       * has two expectations: does the menu display when
 //     // //       * clicked and does it hide when clicked again.
 //     // //       */
         it('menu visibility changes on click', function() {
@@ -85,12 +69,12 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* Created a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
-//         /* TODO: Write a test that ensures when the loadFeed
+//         /* Test ensures that when the loadFeed
 //          * function is called and completes its work, there is at least
 //          * a single .entry element within the .feed container.
-//          * Remember, loadFeed() is asynchronous so this test will require
+//          * loadFeed() is asynchronous so this test requires
 //          * the use of Jasmine's beforeEach and asynchronous done() function.
 //          */
         beforeEach(function(done) {
@@ -99,20 +83,23 @@ $(function() {
             });
         });
         it('contains at least one entry on load', function(done) {
-            expect($('.entry').length).not.toBe(0);
+            expect($('.feed .entry').length).not.toBe(0);
             done();
         });
     });
 
-//     /* TODO: Write a new test suite named "New Feed Selection" */
+//     /* Created a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function() {
-//     //     /* TODO: Write a test that ensures when a new feed is loaded
-//     //      * by the loadFeed function that the content actually changes.
-//     //      * Remember, loadFeed() is asynchronous.
+//     //     /* Test ensures that when a new feed is loaded
+//     //      * by the loadFeed function the content actually changes.
 //     //      */
             var oldFeed;
             var newFeed;
             var originalTimeout;
+
+//     //     /* Research on jasmine.DEFAULT_TIMEOUT_INTERVAL
+//     //      * was conducted here https://jasmine.github.io/2.1/introduction
+//     //      */
 
             beforeEach(function(done) {
                 originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
